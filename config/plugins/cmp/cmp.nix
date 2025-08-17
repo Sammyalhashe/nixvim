9cb64f766be9f8be869514dc1aecbaca32bee83a
Sammy Al Hashemi <sammy@salh.xyz>
2025-08-17 19:43:07 +0000

    Attempting to change cmp insertion.
    I want it to insert on space

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -62,7 +62,7 @@
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
-          "<C-CR>" = "cmp.mapping.confirm({ select = true })";
+          "CR" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert })";
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
         };
       };
