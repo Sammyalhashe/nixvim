741e31157a8557baa094ae060e763d758768cf06
Sammyalhashe <sammy@salh.xyz>
2025-10-16 08:43:59 +0000

    Accidentally had the bool in the wrong way.

--- config/plugins/utils/obsidian.nix
+++ config/plugins/utils/obsidian.nix
@@ -6,7 +6,7 @@
 }:
 let
   notesPath =
-    if !config.nixvim.wsl then "/mnt/c/Users/sammy/Syncthing/My notes" else "~/todo_somewhere";
+    if config.nixvim.wsl then "/mnt/c/Users/sammy/Syncthing/My notes" else "~/todo_somewhere";
 in
 {
   plugins.obsidian = {
