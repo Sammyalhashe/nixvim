009f75137b8842b103173ef211c15d1648da44b8
Sammyalhashe <sammy@salh.xyz>
2026-05-12 12:27:35 -0400

    [plugins] Fixed lazyJui config.

--- config/plugins/git/lazyjui.nix
+++ config/plugins/git/lazyjui.nix
@@ -11,36 +11,44 @@ let
       rev = "main";
       hash = "sha256-zvNlR833ztEzEKZcqGZ1sIKf2yLo6RqmPRTnn4vBfaM=";
     };
+    postInstall = ''
+      rm -f $out/lua/lazyjui.lua
+    '';
   };
 in
 {
   extraPlugins = [ lazyjui-nvim ];
 
   extraPackages = with pkgs; [
-    jj
+    jujutsu
     jjui
   ];
 
   extraConfigLua = ''
-    require('lazyjui').setup({
-      cmd = { "jjui" },
-      height = 0.8,
-      width = 0.9,
-      winblend = 0,
-      use_default_keymaps = true,
-      border = {
-        chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-        thickness = 0,
-        winhl_str = "",
-      },
-    })
+    local ok, lazyjui = pcall(require, "lazyjui")
+    if ok then
+      lazyjui.setup({
+        cmd = { "jjui" },
+        height = 0.8,
+        width = 0.9,
+        winblend = 0,
+        use_default_keymaps = true,
+        border = {
+          chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+          thickness = 0,
+          winhl_str = "",
+        },
+      })
+    else
+      vim.notify("lazyjui failed to load: " .. tostring(lazyjui), vim.log.levels.ERROR)
+    end
   '';
 
   keymaps = [
     {
       mode = "n";
       key = "<leader>gj";
-      action = "<cmd>LazyJUI<CR>";
+      action = "<cmd>LazyJui<CR>";
       options = {
         desc = "LazyJUI (jjui)";
       };
