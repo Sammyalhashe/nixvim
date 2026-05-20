64342629e490f88a62f300db5fe4daa3c9240949
Sammyalhashe <sammy@salh.xyz>
2026-05-20 16:45:10 -0400

    [plugins] Remove `markdown-preview` and disable preview for `Markview` by default.

--- config/plugins/utils/markview.nix
+++ config/plugins/utils/markview.nix
@@ -2,7 +2,12 @@ _: {
   plugins.markview = {
     enable = true;
     settings = {
-
+      preview = {
+        enable = true;
+      };
+      extraConfig = ''
+        vim.api.nvim_set_keymap("n", "<leader>s", "<CMD>Markview splitToggle<CR>", { desc = "Toggles `splitview` for current buffer"})
+      '';
     };
   };
 }
